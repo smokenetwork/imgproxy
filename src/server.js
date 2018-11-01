662e89ba83e87992441713fb92fbc093db9ec7ca
Tuan Pham Anh <baabeetaa@gmail.com>
2018-11-01 21:28:02 +0700

    normalizeUrl

---
+++ src/server.js
@@ -0,0 +1,86 @@
+const express = require('express');
+const path = require('path');
+const sharp = require('sharp');
+const app = express();
+const request = require('request');
+
+let router = express.Router();
+
+client_error_handler = (err, req, res, next) => {
+  if (req.xhr) {
+    res.status(500).send({ error: 'Something failed!' })
+  } else {
+    next(err)
+  }
+};
+
+error_handler = (err, req, res, next) => {
+  if (res.headersSent) {
+    return next(err)
+  }
+  res.set('content-type', 'image/png');
+  res.set('Cache-Control', 'public,max-age=600,immutable'); // cache 10 min
+  res.sendFile('error.png', { root: path.join(__dirname, '../assets') });
+
+  // console.error(err.stack)
+  // res.status(500).send('Something broke!');
+};
+
+// router.get('/:width(\\d+)x:height(\\d+)/:url(.*)', proxyHandler)
+router.get('/:width(\\d+)x:height(\\d+)/*?', (req, res, next) => {
+  let {width, height} = req.params;
+  let url = req.params[0];
+
+  if (width) {
+    width = parseInt(width);
+    if (width === 0) {
+      width = null;
+    }
+    if (width > 1024) {
+      width = 1024;
+    }
+  }
+
+  if (height) {
+    height = parseInt(height);
+    if (height === 0) {
+      height = null;
+    }
+    if (height > 2048) {
+      height = 2048;
+    }
+  }
+
+  let transform = sharp();
+  transform.on('error', function(err) {
+    next(err);
+  });
+
+  if (width || height) {
+    transform = transform.resize(width, height);
+  }
+
+  // readStream.pipe(transform).pipe(res);
+  request.get(url)
+    .on('response', function(url_res) {
+
+
+      //////////
+      const content_type = url_res.headers["content-type"];
+      const accepted_content_types = [ 'image/gif', 'image/jpeg', 'image/png', 'image/webp'];
+      if (!accepted_content_types.includes(content_type)) {
+        next(new Error("Unsupported content-type"));
+      }
+      res.set('content-type', content_type);
+      res.set('Cache-Control', 'public,max-age=29030400,immutable');
+    })
+    .pipe(transform)
+    .pipe(res);
+});
+
+app.use('/', router);
+app.use(client_error_handler);
+app.use(error_handler); // define error-handling middleware last, after other app.use() and routes calls
+app.listen(8000, () => {
+  console.log('Server started!');
+});
---
+++ src/uri_normalize.js
@@ -0,0 +1,45 @@
+// const { URL } = require('url');
+const normalizeUrl = require('normalize-url');
+
+const BASE_URL = "https://base.com/imageproxy/";
+
+normalize = (uri_str) => {
+  let result_url = uri_str;
+  const opts = {stripWWW: false};
+
+  try {
+    if (uri_str.length > 256) {
+      throw new Error("invalid url");
+    }
+
+    const idx_of_http = uri_str.lastIndexOf('http://');
+    const idx_of_https = uri_str.lastIndexOf('https://');
+
+    const idx = Math.max(idx_of_http, idx_of_https);
+    if (idx === -1) {
+      throw new Error("invalid url");
+    }
+    const uri = uri_str.slice(idx);
+
+    result_url = normalizeUrl(uri, opts);
+
+    if (result_url.startsWith(BASE_URL)) {
+      throw new Error("invalid url");
+    }
+  } catch (e) {
+    result_url = null;
+  }
+
+  console.log(`before: ${uri_str}`);
+  console.log(`after : ${result_url}`);
+
+  return result_url;
+};
+
+test_normalize = () => {
+  normalize('https://www.x/y/http://a.com/b/d//e/g?b=1&a=2');
+  normalize('http://a.com/https://base.com/imageproxy/b/d//e/g?b=1&a=2');
+};
+
+
+test_normalize();
